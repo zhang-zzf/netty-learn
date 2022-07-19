0d22bee0802c21511c41e59c4668467b0c7612bf
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-07-19 14:50:24 +0800

    feat: add SingleThreadTopicFilter and UT

---
+++ 20220706/src/main/java/org/example/mqtt/broker/jvm/SingleThreadTopicFilter.java
@@ -0,0 +1,222 @@
+package org.example.mqtt.broker.jvm;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.example.mqtt.broker.TopicFilter;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.*;
+
+/**
+ * @author 张占峰 (Email: zhang.zzf@alibaba-inc.com / ID: 235668)
+ * @date 2022/7/13
+ */
+@Slf4j
+public class SingleThreadTopicFilter implements TopicFilter {
+
+    public static final String LEVEL_SEPARATOR = "/";
+    public static final String MULTI_LEVEL_WILDCARD = "#";
+    public static final String SINGLE_LEVEL_WILDCARD = "+";
+
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    private final Object EMPTY = new Object();
+    private final ConcurrentMap<String, Object> preciseTopicFilter = new ConcurrentHashMap<>();
+
+    // fuzzy TopicFilter tree
+    private final Node root = new Node("*", null);
+
+    @Override
+    public Set<String> match(String topicName) {
+        Set<String> fuzzyMatch = fuzzyMatch(topicName);
+        if (preciseMatch(topicName)) {
+            fuzzyMatch.add(topicName);
+        }
+        return fuzzyMatch;
+    }
+
+    @SneakyThrows
+    @Override
+    public void add(String topicFilter) {
+        if (!validate(topicFilter)) {
+            throw new IllegalArgumentException("TopicFilter is illegal: " + topicFilter);
+        }
+        if (!isFuzzyTopic(topicFilter)) {
+            preciseTopicFilter.put(topicFilter, EMPTY);
+        } else {
+            Runnable addTask = () -> addToFuzzyTopicTree(topicFilter);
+            // wait until add op is done
+            executorService.submit(addTask).get();
+        }
+    }
+
+    private void addToFuzzyTopicTree(String topicFilter) {
+        String[] topicLevels = topicFilter.split(LEVEL_SEPARATOR);
+        Node parent = root;
+        for (int i = 0; i < topicLevels.length; i++) {
+            String level = topicLevels[i];
+            Node n = lastLevel(i, topicLevels) ? new Node(level, topicFilter) : new Node(level);
+            parent = parent.addChild(n);
+        }
+    }
+
+    private boolean validate(String topicFilter) {
+        if (topicFilter == null) {
+            return false;
+        }
+        int idx;
+        if ((idx = topicFilter.indexOf(MULTI_LEVEL_WILDCARD)) != -1 && idx != topicFilter.length() - 1) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isFuzzyTopic(String topicFilter) {
+        return topicFilter.contains(MULTI_LEVEL_WILDCARD) || topicFilter.contains(SINGLE_LEVEL_WILDCARD);
+    }
+
+    @SneakyThrows
+    @Override
+    public void remove(String topicFilter) {
+        if (topicFilter == null) {
+            throw new NullPointerException();
+        }
+        if (!isFuzzyTopic(topicFilter)) {
+            preciseTopicFilter.remove(topicFilter);
+        } else {
+            Runnable removeTask = () -> dfsRemove(topicFilter.split(LEVEL_SEPARATOR), 0, root);
+            // wait util remove op is done
+            executorService.submit(removeTask).get();
+        }
+    }
+
+    private void dfsRemove(String[] topicLevels, int levelIdx, Node parent) {
+        if (levelIdx >= topicLevels.length) {
+            return;
+        }
+        String topicLevel = topicLevels[levelIdx];
+        Node n = parent.child(topicLevel);
+        if (n == null) {
+            return;
+        }
+        if (lastLevel(levelIdx, topicLevels)) {
+            n.topic(null);
+        } else {
+            dfsRemove(topicLevels, levelIdx + 1, n);
+        }
+        // try clean child node if needed.
+        if (n.canDelete()) {
+            parent.removeChild(n);
+        }
+    }
+
+    private Set<String> fuzzyMatch(String topicName) {
+        Set<String> ret = new HashSet<>(4);
+        if (topicName == null || topicName.isEmpty()) {
+            return ret;
+        }
+        dfsFuzzyMatch(topicName.split(LEVEL_SEPARATOR), 0, root, ret);
+        return ret;
+    }
+
+    private void dfsFuzzyMatch(String[] topicLevels, int levelIdx, Node parent, Set<String> ret) {
+        if (levelIdx >= topicLevels.length) {
+            return;
+        }
+        String topicLevel = topicLevels[levelIdx];
+        Node n;
+        if ((n = parent.child(topicLevel)) != null) {
+            if (lastLevel(levelIdx, topicLevels)) {
+                addNode(ret, n);
+            }
+            dfsFuzzyMatch(topicLevels, levelIdx + 1, n, ret);
+        }
+        if ((n = parent.child(MULTI_LEVEL_WILDCARD)) != null) {
+            addNode(ret, n);
+        }
+        if ((n = parent.child(SINGLE_LEVEL_WILDCARD)) != null) {
+            if (lastLevel(levelIdx, topicLevels)) {
+                addNode(ret, n);
+            }
+            dfsFuzzyMatch(topicLevels, levelIdx + 1, n, ret);
+        }
+    }
+
+    private void addNode(Set<String> ret, Node n) {
+        if (n.topic() != null) {
+            ret.add(n.topic());
+        }
+    }
+
+    private boolean lastLevel(int level, String[] levelArray) {
+        return level == levelArray.length - 1;
+    }
+
+    private boolean preciseMatch(String topicName) {
+        return preciseTopicFilter.containsKey(topicName);
+    }
+
+    public static class Node {
+
+        private final String level;
+        /**
+         * null 表示本节点不是 topicFilter
+         */
+        private volatile String topicFilter;
+        /**
+         * child Nodes
+         */
+        private final ConcurrentMap<String, Node> childNodes = new ConcurrentHashMap<>();
+
+        public Node(String level, String topicFilter) {
+            this.level = level;
+            this.topicFilter = topicFilter;
+        }
+
+        public Node(String level) {
+            this(level, null);
+        }
+
+        public String topic() {
+            return this.topicFilter;
+        }
+
+        public Node topic(String topicFilter) {
+            this.topicFilter = topicFilter;
+            return this;
+        }
+
+        public boolean canDelete() {
+            return this.topicFilter == null && this.childNodes.isEmpty();
+        }
+
+        public Node addChild(Node child) {
+            Node nextNode;
+            if ((nextNode = childNodes.putIfAbsent(child.level, child)) == null) {
+                nextNode = child;
+            } else {
+                // the child Node is a TopicFilter. must update the exist node in the lock.
+                if (child.topic() == null) {
+                    nextNode.topic(child.topic());
+                }
+            }
+            return nextNode;
+        }
+
+        public void removeChild(Node node) {
+            if (node.canDelete()) {
+                // important: must check child node status before remove it from parent.childNodes
+                this.childNodes.remove(node.level, node);
+            }
+        }
+
+        public Node child(String level) {
+            return childNodes.get(level);
+        }
+
+    }
+
+}
+
+
---
+++ 20220706/src/test/java/org/example/mqtt/broker/jvm/SingleThreadTopicFilterTest.java
@@ -0,0 +1,320 @@
+package org.example.mqtt.broker.jvm;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.mqtt.broker.TopicFilter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.BDDAssertions.then;
+
+/**
+ * @author 张占峰 (Email: zhang.zzf@alibaba-inc.com / ID: 235668)
+ * @date 2022/7/15
+ */
+@Slf4j
+class SingleThreadTopicFilterTest {
+
+    TopicFilter tf;
+
+    @BeforeEach
+    public void beforeEach() {
+        tf = new SingleThreadTopicFilter();
+    }
+
+    /**
+     * 不包含 wildcards 的 TopicFilter 流程 UT
+     * <p>正向测试流程：测试接口语义的正确性</p>
+     */
+    @Test
+    void givenDefaultTopicFilter_whenAddPrecise_thenSuccess() {
+        String topicFilter = "a/b";
+        tf.add(topicFilter);
+        Set<String> match = tf.match(topicFilter);
+        then(match).contains(topicFilter);
+    }
+
+    /**
+     * 包含 wildcards 的 TopicFilter 流程 UT
+     * <p>正向测试流程：测试接口语义的正确性</p>
+     */
+    @Test
+    void givenDefaultTopicFilter_whenAddWildcardsTopicFilter_thenSuccess() {
+        String topicFilter = "a/#";
+        tf.add(topicFilter);
+        Set<String> match = tf.match("a/b");
+        then(match).contains(topicFilter);
+    }
+
+    /**
+     * 包含 wildcards 的 TopicFilter 流程 UT
+     * <p>正向测试流程：测试接口语义的正确性</p>
+     * <p>TopicFilter: a/b/#; Topic: a/b -> not Match </p>
+     */
+    @Test
+    void givenDefaultTopicFilter_whenAddWildcardsTopicFilter2_thenMatchFailed() {
+        String topicFilter = "a/b/#";
+        tf.add(topicFilter);
+        Set<String> match = tf.match("a/b");
+        then(match).isEmpty();
+    }
+
+    /**
+     * 包含 wildcards 的 TopicFilter 流程 UT
+     * <p>TopicFilter 非法测试</p>
+     * <p>TopicFilter: a/b/#/b -> IllegalArgumentException </p>
+     */
+    @Test
+    void givenDefaultTopicFilter_whenAddIllegalTopicFilter_thenThrowException() {
+        Throwable throwable = catchThrowable(() -> tf.add("a/b/#/c"));
+        then(throwable).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    /**
+     * 包含 wildcards 的 TopicFilter 流程 UT
+     * <p>正向测试流程：测试接口语义的正确性</p>
+     * <p>TopicFilter: /a/#</p>
+     * <p>Topic: /a/b -> Match </p>
+     * <p>Topic: a/b -> not Match </p>
+     */
+    @Test
+    void givenDefaultTopicFilter_whenAddWildcardsTopicFilter3_thenMatchSuccess() {
+        String topicFilter = "/a/#";
+        tf.add(topicFilter);
+        then(tf.match("/a/b")).contains(topicFilter);
+        then(tf.match("a/b")).isEmpty();
+    }
+
+    /**
+     * 包含 wildcards 的 TopicFilter 流程 UT
+     * <p>正向测试流程：测试接口语义的正确性</p>
+     * <p>TopicFilter: /a/#</p>
+     * <p>Topic: /a/b -> Match </p>
+     * <p>Topic: a/b -> not Match </p>
+     */
+    @Test
+    void givenDefaultTopicFilter_whenAddWildcardsTopicFilter4_thenMatchSuccess() {
+        String topicFilter = "/a/#";
+        tf.add(topicFilter);
+        then(tf.match("/a/b")).contains(topicFilter);
+        then(tf.match("a/b")).isEmpty();
+    }
+
+    /**
+     * 包含 wildcards 的 TopicFilter 流程 UT
+     * <p>正向测试流程：测试接口语义的正确性</p>
+     * <p>add TopicFilter: a/b/#</p>
+     * <p>topic: a/b -> not match </p>
+     * <p>add TopicFilter: a/+</p>
+     * <p>topic: a/b -> match </p>
+     */
+    @Test
+    void givenDefaultTopicFilter_whenAddWildcardsTopicFilter5_thenMatchSuccess() {
+        tf.add("a/b/#");
+        then(tf.match("a/b")).isEmpty();
+        tf.add("a/+");
+        then(tf.match("a/b")).contains("a/+");
+    }
+
+    /**
+     * 包含 wildcards 的 TopicFilter 流程 UT
+     * <p>逆向测试流程：测试接口语义的正确性</p>
+     * <p>add TopicFilter: a/#</p>
+     * <p>remove TopicFilter: a/#</p>
+     */
+    @Test
+    void givenDefaultTopicFilter_whenAddWildcardsTopicFilter10_thenMatchSuccess() throws InterruptedException {
+        tf.add("a/#");
+        tf.remove("a/#");
+        then(tf.match("a/b")).contains();
+    }
+
+    /**
+     * 包含 wildcards 的 TopicFilter 流程 UT
+     * <p>逆向测试流程：测试接口语义的正确性</p>
+     * <p>add TopicFilter: a/b/#</p>
+     * <p>remove TopicFilter: a/b/+</p>
+     */
+    @Test
+    void givenDefaultTopicFilter_whenAddWildcardsTopicFilter11_thenMatchSuccess() throws InterruptedException {
+        tf.add("a/b/#");
+        tf.remove("a/b/+");
+        then(tf.match("a/b/c")).contains("a/b/#");
+    }
+
+    /**
+     * 包含 wildcards 的 TopicFilter 流程 UT
+     * <p>逆向测试流程：测试接口语义的正确性</p>
+     * <p>add TopicFilter: a/b/#</p>
+     * <p>remove TopicFilter: a/#</p>
+     */
+    @Test
+    void givenDefaultTopicFilter_whenAddWildcardsTopicFilter12_thenMatchSuccess() throws InterruptedException {
+        tf.add("a/b/#");
+        tf.remove("a/#");
+        then(tf.match("a/b/c")).contains("a/b/#");
+    }
+
+    /**
+     * TopicFilter 1000W 压力测试
+     * <p>分析内存占用</p>
+     * <pre>
+     *     JVM稳定后占用约 9.5G 内存
+     *     - 6G -> 2700W char[] 数组对象 约等于 237 b/字符串 （32*7=224）
+     *     - 600M -> 2700W String 对象（每个 String 对象占用 24 个字节）
+     *     - 3G -> 1600W TopicFilter.Node (约 400M)
+     *          -> 1600W ConcurrentHashMap (约 1G)
+     *          -> 1600W ConcurrentHashMap.Node (约500M)
+     * </pre>
+     * <p>单线程7层分析查询效率</p>
+     * <pre>
+     *     now tf has 10,761,678 topicFilter 1000W
+     *     test 10000000 cnt, use time: 29419322611ns, 2941ns/per
+     *     QPS = 333,000 约 33W/秒的匹配查询
+     * </pre>
+     */
+    @Test
+    void givenTopicFilter_whenAdd10000000_when() throws InterruptedException {
+        Runnable task = () -> {
+            int totalLevel = 7, cntPerLevel = 9;
+            int total = 0;
+            List<Set<String>> topic = new ArrayList<>(totalLevel);
+            for (int i = 0; i < totalLevel; i++) {
+                Set<String> list = new HashSet<>(10);
+                if (i == 0) {
+                    for (int j = 0; j < cntPerLevel; j++) {
+                        list.add(UUID.randomUUID().toString());
+                    }
+                } else {
+                    Set<String> level = topic.get(i - 1);
+                    for (String l : level) {
+                        for (int j = 0; j < cntPerLevel; j++) {
+                            list.add(l + "/" + UUID.randomUUID().toString());
+                        }
+                    }
+                }
+                log.info("curLevel: {} -> topic: {}", i, list.size());
+                topic.add(list);
+                for (String s : list) {
+                    tf.add(s + "/+");
+                    tf.add(s + "/#");
+                    total += 2;
+                }
+                log.info("now tf has {} topicFilter", total);
+            }
+            // test ut
+            long nanoTime = System.nanoTime();
+            Random random = new Random();
+            int testCnt = 100;
+            int totalTestCnt = 0;
+            for (int i = 0; i < testCnt; i++) {
+                int l = random.nextInt(totalLevel);
+                for (String s : topic.get(l)) {
+                    totalTestCnt += 1;
+                    Set<String> match = tf.match(s);
+                    if (l == 0) {
+                        then(match).isEmpty();
+                    } else {
+                        then(match).isNotEmpty();
+                    }
+                }
+            }
+            long useTime = System.nanoTime() - nanoTime;
+            log.info("test {} cnt, use time: {}ns, {}ns/per", totalTestCnt, useTime, useTime / totalTestCnt);
+            // test ut
+            // clear topic
+            topic.clear();
+            topic = null;
+        };
+        new Thread(task).start();
+        Thread.currentThread().join();
+    }
+
+    /**
+     * TopicFilter 1000W 压力测试
+     * <p>分析内存占用</p>
+     * <pre>
+     *     JVM稳定后占用约 9.5G 内存
+     *     - 6G -> 2700W char[] 数组对象 约等于 237 b/字符串 （32*7=224）
+     *     - 600M -> 2700W String 对象（每个 String 对象占用 24 个字节）
+     *     - 3G -> 1600W TopicFilter.Node (约 400M)
+     *          -> 1600W ConcurrentHashMap (约 1G)
+     *          -> 1600W ConcurrentHashMap.Node (约500M)
+     * </pre>
+     * <p>多线程7层分析查询效率</p>
+     * <pre>
+     *     now tf has 10,761,678 topicFilter 1000W
+     *     test 2834307888 cnt, use time: 300008541983ns, 105ns/per
+     *     test 530227043 cnt, use time: 300008434183ns, 565ns/per
+     *     test 350353231 cnt, use time: 300008148483ns, 856ns/per
+     *     test 204356240 cnt, use time: 300007558331ns, 1468ns/per
+     *     test 146508429 cnt, use time: 300008686164ns, 2047ns/per
+     *     test 106183582 cnt, use time: 300007243579ns, 2825ns/per
+     *     test 75040733 cnt, use time: 300007885698ns, 3997ns/per
+     *     300s内共执行了 4246977146 次查询，QPS：14,156,590 约 1400W/s
+     * </pre>
+     */
+    @Test
+    void givenTopicFilter_whenAdd10000000_whenTestMultiThreadRead() throws InterruptedException {
+        int totalLevel = 7, cntPerLevel = 9;
+        int total = 0;
+        List<Set<String>> topic = new ArrayList<>(totalLevel);
+        for (int i = 0; i < totalLevel; i++) {
+            Set<String> list = new HashSet<>(10);
+            if (i == 0) {
+                for (int j = 0; j < cntPerLevel; j++) {
+                    list.add(UUID.randomUUID().toString());
+                }
+            } else {
+                Set<String> level = topic.get(i - 1);
+                for (String l : level) {
+                    for (int j = 0; j < cntPerLevel; j++) {
+                        list.add(l + "/" + UUID.randomUUID().toString());
+                    }
+                }
+            }
+            log.info("curLevel: {} -> topic: {}", i, list.size());
+            topic.add(list);
+            for (String s : list) {
+                tf.add(s + "/+");
+                tf.add(s + "/#");
+                total += 2;
+            }
+            log.info("now tf has {} topicFilter", total);
+        }
+        // test ut
+        CountDownLatch latch = new CountDownLatch(1);
+        for (int i = 0; i < totalLevel; i++) {
+            final Set<String> level = topic.get(i);
+            new Thread(() -> {
+                long nanoTime = System.nanoTime();
+                long totalTestCnt = 0;
+                while (latch.getCount() != 0) {
+                    // test ut
+                    for (String s : level) {
+                        totalTestCnt += 1;
+                        tf.match(s);
+                        if (latch.getCount() == 0) {
+                            break;
+                        }
+                        if (totalTestCnt == Long.MAX_VALUE - 1) {
+                            long useTime = System.nanoTime() - nanoTime;
+                            log.info("test {} cnt, use time: {}ns, {}ns/per", totalTestCnt, useTime, useTime / totalTestCnt);
+                            totalTestCnt = 0;
+                            nanoTime = System.nanoTime();
+                        }
+                    }
+                }
+                long useTime = System.nanoTime() - nanoTime;
+                log.info("test {} cnt, use time: {}ns, {}ns/per", totalTestCnt, useTime, useTime / totalTestCnt);
+            }).start();
+        }
+        Thread.sleep(300000);
+        latch.countDown();
+        Thread.currentThread().join();
+    }
+
+}

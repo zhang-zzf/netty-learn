8ab7c83c4e9c2081a035072597ac5f43f03ba610
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-10-06 17:19:38 +0800

    test: add UT for ControlPacket Bug

--- 20220706/src/test/java/org/example/mqtt/model/ControlPacketTest.java
+++ 20220706/src/test/java/org/example/mqtt/model/ControlPacketTest.java
@@ -1,5 +1,7 @@
 package org.example.mqtt.model;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +17,7 @@ class ControlPacketTest {
     @Test
     void given_whenHexAndDeHex_then() {
         AtomicLong cnt = new AtomicLong(0);
-        for (short i = Short.MIN_VALUE;; i++) {
+        for (short i = Short.MIN_VALUE; ; i++) {
             then(hexPIdToShort(hexPId(i))).isEqualTo(i);
             cnt.getAndIncrement();
             if (i == Short.MAX_VALUE) {
@@ -23,6 +25,16 @@ class ControlPacketTest {
             }
         }
         then(cnt.get()).isEqualTo(65536L);
+    }
+
+    @Test
+    void givenPartTcpPacket_whenDecodeMqttControlPacket_then() {
+        // 0x30f2 -> Publish Packet but just part ot it.
+        ByteBuf buf = Unpooled.buffer(2);
+        buf.writeByte(0x30);
+        buf.writeByte(0xf2);
+        // can not decode a ControlPacket
+        then(ControlPacket.tryPickupPacket(buf)).isEqualTo(-1);
     }
 
 }
